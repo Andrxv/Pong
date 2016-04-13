42f38911f7b87c0f3a673d159a47d126d9b802c0
Andres Villar <andresvillarx@gmail.com>
2016-04-12 23:51:08 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,44 +1,137 @@
-
 var HelloWorldLayer = cc.Layer.extend({
-    jugador1:null,    
-    jugador2:null,    
-    pelota:null,    
-    puntuacion1:null,
-    puntuacion2:null,
-    inicializ
-        this.jugador1 =  new cc.DrawNode();
-        this.jugador1.drawRect(cc.p(0,0),cc.p(20,100),color,3);
-        this.jugador1.setPosition(size.width * 0.1,size.height / 2);
-        this.addChild(this.jugador1, 1);
-
-        this.jugador2 =  new cc.DrawNode();
-        this.jugador2.drawRect(cc.p(0,0),cc.p(20,100),color,3);
-        this.jugador2.setPosition(size.width -(size.width * 0.1),size.height / 2);
-        this.addChild(this.jugador2, 1);        
-
-        var lineaDivisoria =  new cc.DrawNode();
-        lineaDivisoria.drawSegment(cc.p(size.width/2,0),cc.p(size.width/2,size.height),3,color);
-        this.addChild(lineaDivisoria,0);
+    player1:null,    
+    player2:null,    
+    ball:null,
+    sco1:0, //score player 1
+    sco2:0, //score player 2
+    po1:null, //points player 1
+    po2:null, // points player 2
+    move_x:0,
+    move_y:0,
+    speed:0,
+    
+    //Pong Game
+    initializer:function(){
         
-        this.pelota =  new cc.DrawNode();
-        this.pelota.drawCircle(cc.p(0,0),5,0,100,false,10,color);
-        this.pelota.setPosition(size.width / 2,size.height / 2);
-        this.addChild(this.pelota, 1);
+        var size = cc.winSize;
+        var color = cc.color(100,100,100);
+        var white = cc.color(255,255,255);
+        
+        this.po1 = new PointsLabel();
+        this.po1.setPosition(size.width * 0.4, size.height - (size.height * 0.10));
+        this.addChild(this.po1,0);
+        
+        this.po2 = new PointsLabel();
+        this.po2.setPosition(size.width - (size.width * 0.4), size.height - (size.height * 0.10));
+        this.addChild(this.po2,0);
+        
+        var middle =  new cc.DrawNode();
+        middle.drawSegment(cc.p(size.width/2,0),cc.p(size.width/2,size.height),3,color);
+        this.addChild(middle,0);
+    
+        this.move_x = this.random(1,3);
+        this.move_y = this.random(1,3);
+        this.speed = this.random(0.0001,0.001);
+        
+        this.player1 = new cc.Sprite(res.player_png);
+        this.player1.setPosition(size.width * 0.1,size.height / 2);
+        this.addChild(this.player1, 1);
 
-        this.puntuacion1 = new cc.LabelTTF("0","Arial",24);
-        this.puntuacion1.setPosition(size.width * 0.4, size.height - (size.height * 0.10));
-        this.addChild(this.puntuacion1,0);
+        this.player2 = new cc.Sprite(res.player_png);
+        this.player2.setPosition(size.width -(size.width * 0.1),size.height / 2);
+        this.addChild(this.player2, 1);        
+
         
-        this.puntuacion2 = new cc.LabelTTF("0","Arial",24);
-        this.puntuacion2.setPosition(size.width - (size.width * 0.4), size.height - (size.height * 0.10));
-        this.addChild(this.puntuacion2,0);
-        
+        this.ball = new cc.Sprite(res.ball_png);
+        this.ball.setPosition(size.width / 2, this.random(15, size.height - 80));
+        this.addChild(this.ball, 1);
+    
+
+
     },
+    
+    
+    moveControls: function(keyCode, event){
+        
+         random: function getRandomInt(min, max) {
+    	return Math.floor(Math.random() * (max - min + 1)) + min;
+	},
+        
+        var target = event.getCurrentTarget();
+        var size = cc.winSize;
+            
+        if(keyCode == cc.KEY.w){
+            if(target.player1.getPositionY() + 30 < size.height - 80)
+                target.player1.setPosition(target.player1.getPositionX(), target.player1.getPositionY() + 40);
+        }
+        
+        if(keyCode == cc.KEY.s){
+            if(target.player1.getPositionY() - 30 > size.height/2 - size.height/2 + 40)
+                target.player1.setPosition(target.player1.getPositionX(), target.player1.getPositionY() - 40);
+        }
+        
+
+        if(keyCode == cc.KEY.up){
+            if(target.player2.getPositionY() + 30 < size.height - 80)
+                target.player2.setPosition(target.player2.getPositionX(), target.player2.getPositionY() + 40);
+        }
+        
+        if(keyCode == cc.KEY.down){
+            if(target.player2.getPositionY() - 30 > size.height/2 - size.height/2 +40)
+                target.player2.setPosition(target.player2.getPositionX(), target.player2.getPositionY() - 40);
+        }
+    },
+    
+    
+    reset:function(){
+        var size = cc.winSize;
+        this.speed = this.random(0.0001,0.001);
+        this.ball.setPosition(size.width / 2, this.random(0, size.height - 80));
+        this.move_x = this.random(1,3);
+        this.move_y = this.random(1,3);
+        this.po1.setString(this.sco1);
+        this.po2.setString(this.sco2);
+    },
+    
+    moveBall: function(){
+        
+        var position = this.ball.getPosition();
+        
+        if(position.y <= 20 || position.y >= cc.winSize.height - 40){
+            this.move_y *= -1;
+            
+        } else if(position.x <= 0 ){
+            this.sco2++;
+            this.reset();
+        } else if(position.x >= cc.winSize.width){
+            this.sco1++;
+            this.reset();
+        } else if (cc.rectIntersectsRect(this.ball.getBoundingBox(), this.player1.getBoundingBox())){
+            cc.log("collision");
+            this.move_x *= -1;
+        }
+        
+        else if(cc.rectIntersectsRect(this.ball.getBoundingBox(), this.player2.getBoundingBox())){
+            cc.log("collision");
+            this.move_x *= -1;       
+        }
+        
+        var newX = this.ball.getPosition().x + this.move_x;
+        var newY = this.ball.getPosition().y + this.move_y;
+        this.ball.setPosition(newX, newY);
+   
+    
     ctor:function () {
         this._super();
-        this.inicializar();
-
-
+        this.initializer();
+      
+        this.schedule(this.moveBall, this.speed);
+        
+		cc.eventManager.addListener({
+			event: cc.EventListener.KEYBOARD,
+			onKeyPressed:  this.moveControls
+		}, this);
+        
         return true;
     }
 });
